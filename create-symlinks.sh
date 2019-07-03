1339cf18e7ad0286af16b8d6737e19206fbc4afb
Charpi <arnaudcharpentier91@gmail.com>
2019-07-03 17:52:45 +0200

    Add symlinks creation script

---
+++ create-symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/conf/.zshrc ~/.zshrc
+ln -s ~/conf/.tmux.conf ~/.tmux.conf
+ln -s ~/conf/.gitconfig ~/.gitconfig
+ln -s ~/conf/.gitignore_global ~/.gitignore_global
+ln -s ~/conf/init.vim ~/.config/nvim/init.vim
+ln -s ~/conf/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/conf/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
