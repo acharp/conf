52cb6826d819ae84f646bd1f354e67beb70bd70c
Charpi <arnaudcharpentier91@gmail.com>
2020-02-20 14:17:57 +0100

    vscode: add python snippets and a pdb set_trace snippet

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,3 +7,4 @@ ln -s ~/conf/.gitignore_global ~/.gitignore_global
 ln -s ~/conf/init.vim ~/.config/nvim/init.vim
 ln -s ~/conf/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/conf/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s ~/conf/python.json ~/Library/Application\ Support/Code/User/snippets/python.json
